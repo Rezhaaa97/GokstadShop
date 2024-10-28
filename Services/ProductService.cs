be44a28e6a1b4c3a093284ba43e342704e8758e1
Rejwan Raouf <rejwan_raouf@outlook.com>
2024-10-28 21:39:09 +0100

    Last commit

--- Services/ProductService.cs
+++ Services/ProductService.cs
@@ -1,9 +1,11 @@
 
 using System.Net.Http.Json;
+using System.Net.NetworkInformation;
 
 public class ProductService
 {
     private readonly HttpClient _http;
+    private static List<Product> _cachedProducts;
 
     public ProductService(HttpClient http)
     {
@@ -12,11 +14,36 @@ public class ProductService
 
     public async Task<List<Product>> GetProducts()
     {
-        return await _http.GetFromJsonAsync<List<Product>>("https://fakestoreapi.com/products");
+        try
+        {
+            if(_cachedProducts != null)
+            {
+                return _cachedProducts;
+            }
+
+             _cachedProducts = await _http.GetFromJsonAsync<List<Product>>("https://fakestoreapi.com/products");
+            
+            return _cachedProducts;
+        }
+        catch (HttpRequestException ex)
+        {
+            Console.WriteLine($"Request error: {ex.Message}");
+            return new List<Product>();
+        }
+        
     }
 
     public async Task<Product> GetProductById(int id)
     {
-        return await _http.GetFromJsonAsync<Product>($"https://fakestoreapi.com/products/{id}");
+        try
+        {
+            return await _http.GetFromJsonAsync<Product>($"https://fakestoreapi.com/products/{id}");    
+        }
+        catch (HttpRequestException ex)
+        {
+            Console.WriteLine($"Request error: {ex.Message}");
+            return null;
+        }
+        
     }
 }
--- obj/Debug/net8.0/GokstadShop.AssemblyInfo.cs
+++ obj/Debug/net8.0/GokstadShop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GokstadShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06866955e32e4c4cc0c9a17ea66dee21da15bce1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bcce0454eb9c73da93d2ac1fee3cd678dea0068")]
 [assembly: System.Reflection.AssemblyProductAttribute("GokstadShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GokstadShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
